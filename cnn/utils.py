eeb1bec5eb6ec197199739628e5d312674cda23c
sambuddharay <sambuddha.ray.02@gmail.com>
2024-04-11 20:11:16 +0530

    cnn added

---
+++ cnn/utils.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+def team_dictify(players: pd.DataFrame):
+    team_dict = {}
+    for team, team_df in players.groupby('Squad'):
+        team_dict[team] = team_df
+    return team_dict
+
+def squarify(M):
+    (a,b)=M.shape
+    if a>b:
+        padding=((0,0),(0,a-b))
+    else:
+        padding=((0,b-a),(0,0))
+    return np.pad(M,padding,mode='constant',constant_values=0)
+
+def append_X_train(X_train: list, players: pd.DataFrame, goalkeepers: pd.DataFrame):
+    players = players[players['90s']>0]
+    players = players.replace(np.nan, 0)
+    goalkeepers = goalkeepers.replace(np.nan, 0)
+    players_compiled = players.merge(goalkeepers, how='left')
+    players_compiled = players_compiled.replace(np.nan, 0)
+    team_dict = team_dictify(players=players_compiled)
+    for team in team_dict.keys():
+        temp = team_dict[team]
+        temp = temp.drop(columns=['Player', 'Nation', 'Pos', 'Squad', 'Born'])
+        sq = squarify(temp)
+        X_train.append(sq)
