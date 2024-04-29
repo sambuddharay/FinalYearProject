aecbb8500b00e26e420d99df910ab64e951466a7
sambuddharay <sambuddha.ray.02@gmail.com>
2024-04-30 01:33:53 +0530

    cnn model 2 created

--- cnn/utils.py
+++ cnn/utils.py
@@ -15,6 +15,24 @@ def squarify(M):
         padding=((0,b-a),(0,0))
     return np.pad(M,padding,mode='constant',constant_values=0)
 
+def pad(m):
+    a=m.shape[0]
+    padding = ((0,45-a),(0,0))
+    return np.pad(m, padding, mode='constant', constant_values=0)
+
+# def append_X_train(X_train: list, players: pd.DataFrame, goalkeepers: pd.DataFrame):
+#     players = players[players['90s']>0]
+#     players = players.replace(np.nan, 0)
+#     goalkeepers = goalkeepers.replace(np.nan, 0)
+#     players_compiled = players.merge(goalkeepers, how='left')
+#     players_compiled = players_compiled.replace(np.nan, 0)
+#     team_dict = team_dictify(players=players_compiled)
+#     for team in team_dict.keys():
+#         temp = team_dict[team]
+#         temp = temp.drop(columns=['Player', 'Nation', 'Pos', 'Squad', 'Born'])
+#         sq = squarify(temp)
+#         X_train.append(sq)
+
 def append_X_train(X_train: list, players: pd.DataFrame, goalkeepers: pd.DataFrame):
     players = players[players['90s']>0]
     players = players.replace(np.nan, 0)
@@ -25,5 +43,5 @@ def append_X_train(X_train: list, players: pd.DataFrame, goalkeepers: pd.DataFra
     for team in team_dict.keys():
         temp = team_dict[team]
         temp = temp.drop(columns=['Player', 'Nation', 'Pos', 'Squad', 'Born'])
-        sq = squarify(temp)
-        X_train.append(sq)
+        padded = pad(temp)
+        X_train.append(padded)
